cee5afc21ccb9ff277ff2bf633a08a1fc484e6f7
Evan McCullough <webmaster@evanamccullough.com>
2014-01-13 12:19:27 -0500

    added loginviewcontroller

---
+++ ratedate/LoginViewController.h
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.h
+//  ratedate
+//
+//  Created by Evan McCullough on 1/13/14.
+//  Copyright (c) 2014 com.centralvalero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ ratedate/LoginViewController.m
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.m
+//  ratedate
+//
+//  Created by Evan McCullough on 1/13/14.
+//  Copyright (c) 2014 com.centralvalero. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import "ViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    
+    
+    FBLoginView *loginView = [[FBLoginView alloc] init];
+    [self.view addSubview:loginView];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
